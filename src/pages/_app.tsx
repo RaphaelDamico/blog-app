88c532ac5a52b91bcd8399f55ebfd0c4ca7b0fb8
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-07 15:59:09 -0300

    Importando o Header dentro do app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
