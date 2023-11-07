8f4dead250c90021b74b53fe293def8bf432a0d0
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-07 16:16:43 -0300

    Criando a estrutura do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,30 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+import Link from 'next/link';
 
 export function Header() {
     return(
-        <header>
-            <h1>Meu Header</h1>
+        <header className={styles.headerContainer}>
+           <div className={styles.headerContent}>
+                <a>
+                    <Image src={logo} alt="Sujeito Programador Logo"/>
+                </a>
+
+                <nav>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/posts">
+                        <a>Conteúdos</a>
+                    </Link>
+                    <Link href="/sobre">
+                        <a>Quem somos?</a>
+                    </Link>
+                </nav>
+
+                <a className={styles.readyButton} type="button" href="https://sujeitoprogramador.com">COMEÇAR</a>
+           </div>
         </header>
     )
 }
