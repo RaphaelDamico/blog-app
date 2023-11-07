485cc02df057beb2ca78c82799641e9768d40b66
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-07 14:38:16 -0300

    Configurando a fonte do projeto no Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,8 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
-
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main/>
