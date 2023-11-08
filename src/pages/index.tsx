ff4fee318959c14cc08915e9fa5310fb8217c370
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-08 12:56:25 -0300

    Criando parte estática da página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,21 @@ export default function Home() {
     <Head>
       <title>Apaixonado por tecnologia- Sujeito Programador</title>
     </Head>
+    <main className={styles.container}>
+      <div className={styles.containerHeader}>
+        <section>
+          <h1>Levando você ao próximo nível!</h1>
+          <span>Uma plataforma com cursos que vão do zero até o profissional na prática, direto ao ponto aplicando o que usamos no mercado de trabalho.</span>
+          <a>
+            <button>
+              COMEÇAR AGORA!
+            </button>
+          </a>
+
+          <img src="/images/banner-conteudos.png" alt="Conteúdos Sujeito Programador"/>
+        </section>
+      </div>
+    </main>
     </>
   )
 }
