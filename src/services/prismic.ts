cff579db4b543ca91a2ab7b9fb354edfa8949668
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-10 16:40:24 -0300

    Alterando link do prismic

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from '@prismicio/client';
 
 export function getPrismicClient(req?: unknown) {
-    const prismic = Prismic.client('https://app-course.cdn.prismic.io/api/v2', {
+    const prismic = Prismic.client('https://my-blog-api.cdn.prismic.io/api/v2', {
         req,
     })
 
