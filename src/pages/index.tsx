478af9edbe1c758040715f94e08eddbfb839cf2e
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-11-10 17:22:09 -0300

    transformando objeto em propriedade pra utilizar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,29 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 import Image from 'next/image';
-import techsImage from '../../public/images/techs.svg'
+import techsImage from '../../public/images/techs.svg';
+import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import {  RichText } from 'prismic-dom';
 
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobile: string;
+  mobileContent: string;
+  mobileBanner: string;
+  titleWeb: string;
+  webContent: string;
+  webBanner: string;
+}
 
-export default function Home() {
+interface ContentProps {
+  content: Content;
+}
 
+export default function Home( { content }: ContentProps) {
+  
   return (
     <>
       <Head>
@@ -62,4 +80,37 @@ export default function Home() {
   )
 }
 
-export const getStaticProps: GetStaticProps
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home')
+  ]);
+
+  //console.log(response.results[0].data);
+
+  const {
+    title, sub_title, link_action,
+    mobile, mobile_content, mobile_banner,
+    title_web, web_content, web_banner
+  } = response.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobile: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    titleWeb: RichText.asText(title_web),
+    webContent: RichText.asText( web_content),
+    webBanner: web_banner.url,
+  };
+
+  return {
+    props: {
+        content
+    },
+    revalidate: 60 * 2 //A página vai ser gerada a cada 2 minutos
+  }
+}
