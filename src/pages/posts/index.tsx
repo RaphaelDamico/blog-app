fd84d402e025da51df686100e416abfa307d2261
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-12-18 12:17:58 -0300

    Tornando a página posts estática

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,27 @@
-import styles from './styles.module.scss';
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import styles from './styles.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import thumbImg from '../../../public/images/thumb.png';
 import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight } from 'react-icons/fi';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client'; 
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    cover: string;
+    description: string;
+    updatedAt: string
+}
+
+interface PostsProps {
+    posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
     return ( 
         <>
             <Head>
@@ -47,4 +63,39 @@ export default function Posts() {
         </>
     );    
          
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query([
+        Prismic.Predicates.at('document.type', 'post')
+    ], {
+        orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+        fetch: ['post.title', 'post.description', 'post.cover'],
+        pageSize: 3
+    })
+
+    //console.log(JSON.stringify(response, null, 2));
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
+            cover: post.data.cover.url,
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+    
+    return {
+        props: {
+            posts
+        },
+        revalidate: 60 * 30 //Atualiza a cada 30 minutos.
+    }
 }
