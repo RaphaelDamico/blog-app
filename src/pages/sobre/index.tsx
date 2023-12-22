e617dda3dc353d57be33bd5fb553e5c10a934ced
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-12-22 00:04:14 -0300

    Criando Layout da página sobre

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -4,6 +4,7 @@ import Styles from './styles.module.scss';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import { FaYoutube, FaInstagram, FaFacebook,FaLinkedin } from 'react-icons/fa';
 
 type Content = {
     title: string;
@@ -16,22 +17,54 @@ type Content = {
 }
 
 interface ContentProps {
-    content: Content
+    content: Content;
 }
 
 export default function Sobre({ content }: ContentProps) {
     return(
-        <div>
-            <h1>Sujeito Programador</h1>
-        </div>
+        <>
+            <Head>
+                <title>Quem somos? | Sujeito Programador</title>
+            </Head>
+            <main className={Styles.container}>
+                <div className={Styles.containerHeader}>
+                    <section className={Styles.ctaText}>
+                        <h1>{content.title}</h1>
+                        <p>{content.description}</p>
+
+                        <a href={content.youtube}>
+                            <FaYoutube size={40}/>
+                        </a>
+
+                        <a href={content.instagram}>
+                            <FaInstagram size={40}/>
+                        </a>
+
+                        <a href={content.facebook}>
+                            <FaFacebook size={40}/>
+                        </a>
+
+                        <a href={content.linkedin}>
+                            <FaLinkedin size={40}/>
+                        </a>
+                    </section>
+
+                    <img
+                        src={content.banner}
+                        alt="Sobre Sujeito Programador"
+                    />
+                </div>
+            </main>
+        </>
     )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient();
+
     const response = await prismic.query([
         Prismic.Predicates.at('document.type', 'about')
-    ]);
+    ])
 
     const {
         title,
@@ -56,7 +89,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
     return {
         props: {
-
+            content
         }
     }
 }
